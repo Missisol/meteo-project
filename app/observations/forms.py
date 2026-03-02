b27f31f840b22a19167d5333818593f65c67af83
MissisOl <missisololol@gmail.com>
2026-03-02 13:15:52 +0300

    Refactor forms

--- app/observations/forms.py
+++ app/observations/forms.py
@@ -4,69 +4,55 @@ from wtforms import StringField, SubmitField, SelectField, DateField, HiddenFiel
 from wtforms.validators import ValidationError, DataRequired, Length
 
 
-# TODO унифицировать формы - убрать повторение
-class ObservationForm(FlaskForm):
-    created_at = DateField('Дата', default=datetime.date.today)
+# Общие варианты выбора для полей
+CLOUDINESS_CHOICES = [
+    ('clear', 'Ясно'),
+    ('mostly_sunny', 'Преимущественно солнечно'),
+    ('cloudy', 'Переменная облачность'),
+    ('mostly_cloudy', 'Преимущественно облачно'),
+    ('overcast', 'Пасмурно')
+]
+
+PRECIPITATION_CHOICES = [
+    ('none', 'Нет'),
+    ('rain', 'Дождь'),
+    ('snow', 'Снег'),
+    ('sleet', 'Снег с дождем'),
+    ('hail', 'Град')
+]
+
+PRECIPITATION_RATE_CHOICES = [
+    ('none', 'Нет'),
+    ('light', 'Слабые'),
+    ('moderate', 'Умеренные'),
+    ('heavy', 'Сильные')
+]
+
+
+class BaseObservationForm(FlaskForm):
+    """Базовый класс с общими полями для наблюдений."""
     cloudiness = SelectField('Облачность',
-        choices=[
-            ('clear', 'Ясно'),
-            ('mostly_sunny', 'Преимущественно солнечно'),
-            ('cloudy', 'Переменная облачность'),
-            ('mostly_cloudy', 'Преимущественно облачно'),
-            ('overcast', 'Пасмурно')
-        ],
+        choices=CLOUDINESS_CHOICES,
         validators=[DataRequired()])
     precipitation = SelectField('Осадки',
-        choices=[
-            ('none', 'Нет'),
-            ('rain', 'Дождь'),
-            ('snow', 'Снег'),
-            ('sleet', 'Снег с дождем'),
-            ('hail', 'Град')
-        ],
+        choices=PRECIPITATION_CHOICES,
         validators=[DataRequired()])
     precipitation_rate = SelectField('Интенсивность осадков',
-        choices=[
-            ('none', 'Нет'),
-            ('light', 'Слабые'),
-            ('moderate', 'Умеренные'),
-            ('heavy', 'Сильные')
-        ])
+        choices=PRECIPITATION_RATE_CHOICES)
     snow_depth = StringField('Высота снежного покрова, см', 
         validators=[Length(min=0, max=4)])
     comment = StringField('Комментарий')
+
+
+class ObservationForm(BaseObservationForm):
+    """Форма для создания нового наблюдения."""
+    created_at = DateField('Дата', default=datetime.date.today)
     submit = SubmitField('Добавить')
 
-class EditForm(FlaskForm):
+
+class EditForm(BaseObservationForm):
+    """Форма для редактирования наблюдения."""
     id = HiddenField('id', id="editId")
-    cloudiness = SelectField('Облачность',
-        choices=[
-            ('clear', 'Ясно'),
-            ('mostly_sunny', 'Преимущественно солечно'),
-            ('cloudy', 'Переменная облачность'),
-            ('mostly_cloudy', 'Преимущественно облачно'),
-            ('overcast', 'Пасмурно')
-        ],
-        validators=[DataRequired()])
-    precipitation = SelectField('Осадки',
-        choices=[
-            ('none', 'Нет'),
-            ('rain', 'Дождь'),
-            ('snow', 'Снег'),
-            ('sleet', 'Снег с дождем'),
-            ('hail', 'Град')
-        ],
-        validators=[DataRequired()])
-    precipitation_rate = SelectField('Интенсивность осадков',
-        choices=[
-            ('none', 'Нет'),
-            ('light', 'Слабые'),
-            ('moderate', 'Умеренные'),
-            ('heavy', 'Сильные')
-        ])
-    snow_depth = StringField('Высота снежного покрова, см', 
-        validators=[Length(min=0, max=4)])
-    comment = StringField('Комментарий')
     submit = SubmitField('Сохранить')
 
 class EmptyForm(FlaskForm):
